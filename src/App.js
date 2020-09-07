0813ac2ea2404fdce288853ffd63f6ff3333fc18
carlos flores <geop0p3@gmail.com>
2020-09-07 10:54:20 -0700

    Fix Url Linkins

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function PublicRoute({ component: Component, authenticated, ...rest }) {
       {...rest}
       render={(props) => authenticated === false
         ? <Component {...props} />
-        : <Redirect to='/agremiados' />}
+        : <Redirect to='/' />}
     />
   )
 }
